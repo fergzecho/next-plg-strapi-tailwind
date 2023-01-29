3415fb7ae9cfab2784ca4ddba5fd6961c1a0845d
fergzecho <fergz.echo@gmail.com>
2023-01-29 11:08:39 +0800

    Added Logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,55 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark': '#1C1C1C',
+      primary: {"50":"#fffbeb","100":"#fef3c7","200":"#fde68a","300":"#fcd34d","400":"#fbbf24","500":"#f59e0b","600":"#d97706","700":"#b45309","800":"#92400e","900":"#78350f"}
+    },
+    fontFamily: {
+      'body': [
+        'Montserrat', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+        ],
+      'sans': [
+        'Montserrat', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+        ]
+      }
+    
+    
   },
   plugins: [],
 }
